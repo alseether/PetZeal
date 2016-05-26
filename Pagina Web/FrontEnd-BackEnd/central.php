539b67927f7a7c6fb83702af3be4102de7ccf4da
Alvaro Lazaro Sevilla <alseether@gmail.com>
2016-05-26 19:27:46 +0200

    Corregido error en el componente central
    Si el usuario no tiene mascotas, no se carga el listado central, solo el
    carrusel

--- Pagina Web/FrontEnd-BackEnd/central.php
+++ Pagina Web/FrontEnd-BackEnd/central.php
@@ -8,31 +8,58 @@
 			if(isset($_COOKIE["log"]) && $_COOKIE["log"] == true){
 				if(isset($_COOKIE["rol"]) &&  $_COOKIE["rol"] == 'User'){
 					$mascotas = getMascotasUsuario($_COOKIE["idUsu"]);
-					$row = $mascotas->fetch_assoc();
-					$primera = getInfoMascota($row["IDmascota"]);
-					$rowPrimera = $primera->fetch_assoc();
-					echo '<div class="cabecera">'.$rowPrimera["Nombre"].'</div>';
-					echo '<ul class="listado-mascotas">';
-						echo '<li id="subir-publicacion">';
-							echo '<img src="assets/images/imagenRegistroMascota.jpg" alt="pots1" id="subir-publicacion-foto">';
-							echo '<textarea type="text" name="descripcion" placeholder="Descripcion" id="cuadro-descripcion"></textarea>';
-							echo '<a id="foto"  class="boton-peq botonNaranja" href="subirImagen.html">Foto</a>';
-							echo '<a id="subir"  class="boton-peq botonNaranja">Subir</a>';
-						echo '</li>';
-						cargaPublicacionesMascota($row["IDmascota"]);
-					echo '</ul>';
+					if($mascotas->num_rows > 0){
+						$row = $mascotas->fetch_assoc();
+						$primera = getInfoMascota($row["IDmascota"]);
+						$rowPrimera = $primera->fetch_assoc();
+						echo '<div class="cabecera">'.$rowPrimera["Nombre"].'</div>';
+						echo '<ul class="listado-mascotas">';
+							echo '<li id="subir-publicacion">';
+								echo '<img src="assets/images/imagenRegistroMascota.jpg" alt="pots1" id="subir-publicacion-foto">';
+								echo '<textarea type="text" name="descripcion" placeholder="Descripcion" id="cuadro-descripcion"></textarea>';
+								echo '<a id="foto"  class="boton-peq botonNaranja" href="subirImagen.html">Foto</a>';
+								echo '<a id="subir"  class="boton-peq botonNaranja">Subir</a>';
+							echo '</li>';
+							
+								cargaPublicacionesMascota($row["IDmascota"]);
+							
+						echo '</ul>';
+					}
 				}else{
 					$posts = getPostsUsuario($_COOKIE["idUsu"])->fetch_assoc();
-					echo '<div class="cabecera">'.$_COOKIE["nick"].'</div>';
-					echo '<ul class="listado-mascotas">';
-						echo '<li id="subir-publicacion">';
-							echo '<img src="assets/images/imagenRegistroMascota.jpg" alt="pots1" id="subir-publicacion-foto">';
-							echo '<textarea type="text" name="descripcion" placeholder="Descripcion" id="cuadro-descripcion"></textarea>';
-							echo '<a id="foto"  class="boton-peq botonNaranja" href="subirImagen.html">Foto</a>';
-							echo '<a id="subir"  class="boton-peq botonNaranja">Subir</a>';
-						echo '</li>';
-						cargaPostUsuario($_COOKIE["idUsu"]);
-					echo '</ul>';
+					if($posts->num_rows > 0){
+						echo '<div class="cabecera">'.$_COOKIE["nick"].'</div>';
+						echo '<ul class="listado-mascotas">';
+							echo '<li id="subir-publicacion">';
+								echo '<img src="assets/images/imagenRegistroMascota.jpg" alt="pots1" id="subir-publicacion-foto">';
+								echo '<textarea type="text" name="descripcion" placeholder="Descripcion" id="cuadro-descripcion"></textarea>';
+								echo '<a id="foto"  class="boton-peq botonNaranja" href="subirImagen.html">Foto</a>';
+								echo '<a id="subir"  class="boton-peq botonNaranja">Subir</a>';
+							echo '</li>';
+							
+								cargaPostUsuario($_COOKIE["idUsu"]);
+						echo '</ul>';
+					}
+					else{
+						$mascotas = getMascotasUsuario($_COOKIE["idUsu"]);
+						if($mascotas->num_rows > 0){
+							$row = $mascotas->fetch_assoc();
+							$primera = getInfoMascota($row["IDmascota"]);
+							$rowPrimera = $primera->fetch_assoc();
+							echo '<div class="cabecera">'.$rowPrimera["Nombre"].'</div>';
+							echo '<ul class="listado-mascotas">';
+								echo '<li id="subir-publicacion">';
+									echo '<img src="assets/images/imagenRegistroMascota.jpg" alt="pots1" id="subir-publicacion-foto">';
+									echo '<textarea type="text" name="descripcion" placeholder="Descripcion" id="cuadro-descripcion"></textarea>';
+									echo '<a id="foto"  class="boton-peq botonNaranja" href="subirImagen.html">Foto</a>';
+									echo '<a id="subir"  class="boton-peq botonNaranja">Subir</a>';
+								echo '</li>';
+								
+									cargaPublicacionesMascota($row["IDmascota"]);
+								
+							echo '</ul>';
+						}
+					}
 				}
 				cargaCarrusel($_COOKIE["idUsu"]);
 			}else{
