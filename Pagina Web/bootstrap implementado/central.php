dc5ec834d11fcbf08542ffd1be626ad28f5ac2c4
christian229 <crvazque@ucm.es>
2016-06-20 16:33:46 +0200

    Corregido carga mascotas pasa User

--- Pagina Web/bootstrap implementado/central.php
+++ Pagina Web/bootstrap implementado/central.php
@@ -6,31 +6,59 @@
 	$id = $_GET["id"];
 	$hayPosts = $_GET["p"];
 	$cargadoPosts = 1;
+	$contenido = false;
 
-	echo '<div class="col-lg-6 col-md-11 col-sm-11">';
+	echo '<div class="div-cuerpo col-lg-6 col-md-11 col-sm-11">';
 		echo '<div id="panelPostsInicio" class="panel panel-default">';					  
 			if(isset($_COOKIE["log"]) && $_COOKIE["log"] == true){
 				if(isset($_COOKIE["rol"]) &&  $_COOKIE["rol"] == 'User'){//usuario
 					if($id == 0){
 						$mascotas = getMascotasUsuario($_COOKIE["idUsu"]);
-						$id =$_COOKIE["idUsu"];
+						//$id =$_COOKIE["idUsu"];
 					}else{
-						$mascotas =  getMascotasUsuario($id);
+						$mascotas =  getInfoMascota($id);
 					}
-					if($mascotas->num_rows > 0){
-						$row = $mascotas->fetch_assoc();
+					$row = $mascotas->fetch_assoc();
+					if($id == 0){
 						$primera = getInfoMascota($row["IDmascota"]);
 						$rowPrimera = $primera->fetch_assoc();
+					}else{
+						
+						$primera = getInfoMascota($id);
+						$rowPrimera = $primera->fetch_assoc();
+					}
+					/*if($mascotas->num_rows > 0){
+						$contenido = true;
+						$row = $mascotas->fetch_assoc();
+						$primera = getInfoMascota($row["IDmascota"]);
+						$rowPrimera = $primera->fetch_assoc();*/
 						echo '<div class="panel-heading "><h2>'.$rowPrimera["Nombre"].'</h2></div>';
 						echo '<div class="panel-body panelPosts-mascota">';
 							echo '<ul class="media-list">';
+								/*echo '<li class="media">';
+									echo'<div class="media-left">';
+										echo'<a href="info.html?masc=true&id='.$rowPrimera["IDmascota"].'">';
+	        								echo'<img class="media-object img-rounded" width="100" height="100" src="assets/images/imagenRegistroMascota.jpg" alt="posts1">';
+	      								echo'</a>';
+									echo'</div>';
+									
+								echo '</li>';*/
 								echo '<li class="media">';
 									echo'<div class="media-left">';
 										echo'<a href="infoMascota_Usu.html">';
 	        								echo'<img class="media-object img-rounded" width="100" height="100" src="assets/images/imagenRegistroMascota.jpg" alt="posts1">';
 	      								echo'</a>';
 									echo'</div>';
-									
+									echo'<div class="media-body">';
+										echo '<form  method="post" action="subirPublicacion.php?id='.$row["IDmascota"].'" enctype="multipart/form-data">';
+											//echo '<img src="assets/images/imagenRegistroMascota.jpg" alt="pots1" id="subir-publicacion-foto">';
+											echo '<textarea class="form-control" rows="4" type="text" name="descripcion" ></textarea>';
+											//cho '<a id="foto" name="foto" class="boton-peq botonNaranja" href="subirImagen.html">Foto</a>';
+											echo '<input id="botonesHeader" class="btn btn-default btn-md" type="file" name="imagen"></input>';
+											echo '<input type="submit" id="botonesHeader" class="btn btn-success btn-md"></input>';
+										echo '</form>';
+									echo '</div>';
+
 								echo '</li>';		
 								if($id == 0){
 									cargaPublicacionesMascota($row["IDmascota"]);
@@ -39,9 +67,10 @@
 								}
 							echo '</ul>';	
 						echo '</div>';
-					}
+					//}
 				}else{//premium  
 					if($hayPosts == 1){
+						$contenido = true;
 						$posts = getPostsUsuario($_COOKIE["idUsu"]);
 						if($posts->num_rows > 0){
 							echo '<div class="panel-heading"><h2>'.$_COOKIE["nick"].'</h2></div>';
@@ -77,6 +106,7 @@
 					if($cargadoPosts != 0){						
 							$mascotas = getMascotasUsuario($_COOKIE["idUsu"]);
 							if($mascotas->num_rows > 0){
+								$contenido = true;
 								$row = $mascotas->fetch_assoc();
 								if($id == 0){
 									$primera = getInfoMascota($row["IDmascota"]);
@@ -86,8 +116,6 @@
 									$primera = getInfoMascota($id);
 									$rowPrimera = $primera->fetch_assoc();
 								}
-							
-								
 									echo '<div class="panel-heading"><h2>'.$rowPrimera["Nombre"].'</h2></div>';
 									echo '<div class="panel-body panelPosts-mascota">';
 										echo '<ul class="media-list">';
